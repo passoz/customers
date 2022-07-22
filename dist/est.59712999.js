5fd3ae975316461e252c3ab03c773b1535f519e5
Fernando Passos <fernandopassoz@gmail.com>
2022-07-22 18:25:33 -0300

    tailwind removed / scss added

--- dist/est.59712999.js
+++ dist/est.59712999.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"lX4BT":[function(require,module,exports) {
+})({"gnWJF":[function(require,module,exports) {
 "use strict";
 var global = arguments[3];
 var HMR_HOST = null;
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"4M6V8":[function(require,module,exports) {
 
-},{}]},["lX4BT","4M6V8"], "4M6V8", "parcelRequireff49")
+},{}]},["gnWJF","4M6V8"], "4M6V8", "parcelRequire94c2")
 
 //# sourceMappingURL=est.59712999.js.map
