694f270e9a84d498504d5eeb6391c344aa79572f
Fernando Passos <fernandopassoz@gmail.com>
2022-07-25 12:00:24 -0300

    Fixed header / contacts added && gp

--- dist/est.59712999.js
+++ dist/est.59712999.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"gnWJF":[function(require,module,exports) {
+})({"7Y4S2":[function(require,module,exports) {
 "use strict";
 var global = arguments[3];
 var HMR_HOST = null;
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"4M6V8":[function(require,module,exports) {
 
-},{}]},["gnWJF","4M6V8"], "4M6V8", "parcelRequire94c2")
+},{}]},["7Y4S2","4M6V8"], "4M6V8", "parcelRequire94c2")
 
 //# sourceMappingURL=est.59712999.js.map
